90886ea24f8f2b2ab84cbde7adcad913ef10d60c
Tyler Krupicka <tylerkrupicka@gmail.com>
2019-04-17 22:59:40 -0700

    Switch to class

---
+++ Keepon.h
@@ -0,0 +1,20 @@
+enum SideMove{
+  CYCLE,
+  CENTER_FROM_LEFT,
+  RIGHT,
+  CENTER_FROM_RIGHT,
+  LEFT
+};
+enum PONMove{
+  UP,
+  HALF_DOWN,
+  DOWN,
+  HALF_UP
+};
+
+enum Mode{
+  DANCE,
+  TOUCH,
+  TEMPO,
+  SLEEP
+};
--- openFrameworks/README.md
+++
@@ -1,3 +0,0 @@
-GUI controller for MyKeepon, written using openFrameworks, and the following addon:
-
-- ofxUI (https://github.com/rezaali/ofxUI)
